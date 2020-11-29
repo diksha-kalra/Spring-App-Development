8c26c47b43e0d8637ed77c182341516ceea32bab
diksha-kalra <kalradiksha11@gmail.com>
2020-11-29 22:53:16 +0530

    Added: spring app for hello messages using http methods

---
+++ src/main/java/com/cg/springapp/Spring.java
@@ -0,0 +1,12 @@
+package com.cg.springapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Spring {
+	public static void main(String[] args) {
+		SpringApplication.run(Spring.class, args);
+	}
+}
